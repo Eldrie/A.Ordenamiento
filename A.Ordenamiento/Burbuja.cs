b1c13f87a0eb8fc1019144ce2dd2cdee85b2275d
Edgar Manuel García González <manolo161070@hotmail.com>
2015-04-20 12:30:38 -0500

    Clase Burbuja

---
+++ A.Ordenamiento/Burbuja.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.Ordenamiento
+{
+    class Burbuja
+    {
+        public static void Aplicar(int[] numeros) 
+        {
+            for (int i = 1; i < numeros.Length; i++)
+            {
+                for (int j = 0; j < numeros.Length -1 ; j++)
+                {
+                    if(numeros[j] > numeros[j + 1])
+                    {
+                        int auxiliar = numeros[j];
+                        numeros[j] = numeros[j + 1];
+                        numeros[j + 1] = auxiliar;
+                    }
+                }
+            }
+        }
+    }
+}
--- A.Ordenamiento/Program.cs
+++ A.Ordenamiento/Program.cs
@@ -10,6 +10,33 @@ namespace A.Ordenamiento
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            string n = Console.ReadLine();
+            int tamano = Int32.Parse(n);
+            int[] numeros = new int[tamano];
+
+            for (int i = 0; i < tamano; i++)
+            {
+                numeros[i] = random.Next(0,100);
+            }
+
+            imprime(numeros);
+
+            Burbuja.Aplicar(numeros);
+
+            imprime(numeros);
+
+            Console.Read();
+        }
+
+        static void imprime(int[] n) 
+        {
+            Console.WriteLine("___________________________");
+
+            for (int i = 0; i < n.Length; i++)
+            {
+                Console.WriteLine(n[i].ToString() + " ");
+            }
         }
     }
 }
